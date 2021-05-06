06ed826f14ac10b3636b2a7c49adceac9512bad9
Sameer Khan <samsam23122001@gmail.com>
2021-05-06 14:13:28 -0400

    Code structure setup added needs api
    File structure with functions setup, some needs implementation with API

--- flinks-training-demo/ClientApp/src/app/login/login.component.ts
+++ flinks-training-demo/ClientApp/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { AuthorizationService } from '../services/authorization.service';
+import { FetchDataComponent } from '../fetch-data/fetch-data.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +10,36 @@ import { Component } from '@angular/core';
 })
 
 export class LoginComponent {
+  @Input() username: string;
+  @Input() password: string;
+  error: boolean = false;
+
+  constructor(private authorizationService:AuthorizationService) {}
+
+  // Set Dynamic Classes
+  setClasses() {
+    let classes = {
+      'error': this.error,
+    }
+    return classes;
+  }
+  /*
+  onLogin(){
+    console.log(this.username, this.password);
+    this.authorizationService.onLogin(this.username, this.password).subscribe(request => {
+      console.log(request);
+    })
+  }*/
+
+ 
+  onLogin(){
+    console.log(this.username, this.password);
+    if (this.authorizationService.onLogin(this.username, this.password) !== "203"){
+      this.error = true;
+    }
+    console.log(this.authorizationService.onLogin(this.username, this.password));
+  }
+
+
+
 }
