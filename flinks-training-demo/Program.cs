92dc1d3deb5db586bfd0fa48dc69800eb6238d9d
simo4-4 <59541717+simo4-4@users.noreply.github.com>
2021-05-06 18:20:38 +0000

    nm

--- flinks-training-demo/Program.cs
+++ flinks-training-demo/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -52,7 +53,7 @@ namespace flinks_training_demo
             
             Console.WriteLine(resp2);
 
-           // CreateHostBuilder(args).Build().Run();
+           CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
