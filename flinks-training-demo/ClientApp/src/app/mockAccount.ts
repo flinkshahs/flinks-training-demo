eeddeab510f4884d77ebfb48718994852e7da8a0
sehr m <smoosabhoy@flinks.com>
2021-05-06 14:48:08 -0400

    added in bootstrap

--- flinks-training-demo/ClientApp/src/app/mockAccount.ts
+++ flinks-training-demo/ClientApp/src/app/mockAccount.ts
@@ -2,12 +2,12 @@ import {Account} from './summary/summary.service'
 
 //Account instance to check functionality
 export const A: Account ={
-    transitNumber: null,
+    transitNumber: 35032,
     institutionNumber: 203,
     availableBalance: 330,
     limitBalance: 600,
     holder: "Flinks Person",
-    overdraftLimit: 0,
+    overdraftLimit: 80,
     title: "Chequing CAD",
     category: "Chequing",
     accountNumber: "1111000",
